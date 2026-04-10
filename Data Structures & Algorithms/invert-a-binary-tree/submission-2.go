b6972da15b22fa9e15ed80cb7d2339a4bd4ee3f5
vikky129 <89781539+vikky129@users.noreply.github.com>
2026-04-10 15:49:13 +0530

    Add: invert-a-binary-tree - submission-2

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-2.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func invertTree(root *TreeNode) *TreeNode {
+    invert(root)
+	return root
+}
+
+func invert(root *TreeNode)  {
+	if root == nil {
+		return
+	}
+	root.Left, root.Right = root.Right, root.Left
+	invert(root.Left)
+	invert(root.Right)
+}
