16b02cbcf4e59366e8d5568266e2cad7af08262c
vikky129 <89781539+vikky129@users.noreply.github.com>
2026-04-14 20:06:39 +0530

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.go
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func rightSideView(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+	
+    li := []int{}
+	mp := map[int]struct{}{}
+
+	li = append(li, root.Val)
+	mp[0] = struct{}{}
+
+	return print(root, li, mp, 0)
+}
+
+func print(root *TreeNode, li []int, mp map[int]struct{}, level int) []int {
+	if root == nil {
+		return li
+	}
+	if _, ok := mp[level]; !ok {
+		li = append(li, root.Val)
+		mp[level] = struct{}{}
+	}
+
+	li = print(root.Right, li, mp, level + 1)
+	li = print(root.Left, li, mp, level + 1)
+
+	return li
+}
